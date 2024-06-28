55d652b1a0c8e9abd5d5d5853c17d074c88c3b9f
WenyXu <wenymedia@gmail.com>
2024-06-28 07:42:17 +0000

    wip

--- src/common/meta/src/distributed_time_constants.rs
+++ src/common/meta/src/distributed_time_constants.rs
@@ -12,7 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::sync::{Mutex, Once};
 use std::time::Duration;
+
+use once_cell::sync::Lazy;
 
 /// Heartbeat interval time (is the basic unit of various time).
 pub const HEARTBEAT_INTERVAL_MILLIS: u64 = 3000;
@@ -40,3 +43,66 @@ pub const META_KEEP_ALIVE_INTERVAL_SECS: u64 = META_LEASE_SECS / 2;
 
 /// The default mailbox round-trip timeout.
 pub const MAILBOX_RTT_SECS: u64 = 1;
+
+/// A global instance of `TimeConstants` initialized lazily.
+pub static GLOBAL_TIME_CONSTANTS: Lazy<TimeConstants> =
+    Lazy::new(|| *CONFIG_TIME_CONSTANTS.lock().unwrap());
+
+static CONFIG_TIME_CONSTANTS: Lazy<Mutex<TimeConstants>> =
+    Lazy::new(|| Mutex::new(TimeConstants::default()));
+
+/// Constants representing various time durations used in the system.
+#[derive(Debug, Clone, Copy)]
+pub struct TimeConstants {
+    /// Heartbeat interval time for datanodes, used as the basic unit for various time calculations.
+    pub datanode_heartbeat_interval: Duration,
+    /// Heartbeat interval time for the frontend.
+    pub frontend_heartbeat_interval: Duration,
+    /// The lease timeout for the metasrv leader.
+    pub meta_leader_lease_timeout: Duration,
+    /// The timeout duration for mailbox message round-trips.
+    pub mailbox_message_round_trip_timeout: Duration,
+}
+
+impl TimeConstants {
+    /// Returns the meta leader keep-alive interval, which is half of the meta leader lease duration.
+    pub fn meta_leader_keep_alive_interval(&self) -> Duration {
+        self.meta_leader_lease_timeout / 2
+    }
+
+    /// Returns the lease duration for flownodes.
+    ///
+    /// The lease seconds of a region. It's set by 3 heartbeat intervals (`datanode_heartbeat_interval` × 3),
+    /// plus some extra buffer (1 second).
+    pub fn flownode_lease(&self) -> Duration {
+        self.datanode_heartbeat_interval * 3 + Duration::from_secs(1)
+    }
+
+    /// Returns the lease duration for datanodes.
+    ///
+    /// The lease seconds of a region. It's set by 3 heartbeat intervals (`datanode_heartbeat_interval` × 3),
+    /// plus some extra buffer (1 second).
+    pub fn datanode_lease(&self) -> Duration {
+        self.datanode_heartbeat_interval * 3 + Duration::from_secs(1)
+    }
+}
+
+impl Default for TimeConstants {
+    fn default() -> Self {
+        TimeConstants {
+            datanode_heartbeat_interval: Duration::from_secs(3),
+            frontend_heartbeat_interval: Duration::from_secs(18),
+            meta_leader_lease_timeout: Duration::from_secs(3),
+            mailbox_message_round_trip_timeout: Duration::from_secs(1),
+        }
+    }
+}
+
+/// Initialize the global [TimeConstants]
+pub fn init_global_time_constants(constants: TimeConstants) {
+    static START: Once = Once::new();
+    START.call_once(move || {
+        let mut c = CONFIG_TIME_CONSTANTS.lock().unwrap();
+        *c = constants;
+    });
+}
